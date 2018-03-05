8f983fe3128636382ae1516c61af9d406a027455
enoonIT <enoonIT@users.noreply.github.com>
2018-03-05 10:49:18 +0100

    added logger support and argparse

--- src/dann_train.py
+++ src/dann_train.py
@@ -16,7 +16,10 @@ as target datasets.
 
 Author: Daniel Bartolomé Rojas (d.bartolome.r@gmail.com)
 '''
+import argparse
+
 import numpy as np
+import time
 
 import torch
 import torch.nn as nn
@@ -25,24 +28,53 @@ import torch.nn.functional as F
 from torch.autograd import Variable, Function
 
 from evaluation import eval_clf
+from logger import Logger
 from models import DANN
 from data_handling import batch_generator, data_loader
-   
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lr', type=float, default=1e-2)
+    # parser.add_argument('--image_size', type=int, default=28)
+    parser.add_argument('--batch_size', default=50, type=int)
+    parser.add_argument('--num_src_epochs', default=15, type=int, help="number of epochs to pre-train source only model")
+    parser.add_argument('--epochs', default=30, type=int)
+    # parser.add_argument('--DANN_weight', default=1.0, type=float)
+    parser.add_argument('--use_deco', action="store_true", help="If true use deco architecture")
+    parser.add_argument('--suffix', help="Will be added to end of name")
+    parser.add_argument('--source', default="mnist")
+    parser.add_argument('--target', default="usps")
+    return parser.parse_args()
+
+
+def get_name(args):
+    name = "lr:%g_batchSize:%d_epochs:%d" % (args.lr, args.batch_size, args.epochs)
+    if args.use_deco:
+        name += "_deco"
+    return name + args.suffix + "_%d" % (time.time() % 100)
+
+
+args = get_args()
+run_name = get_name(args)
+logger = Logger("logs/" + run_name)
 # parameters
-learning_rate = 1e-2          # learning rate
-num_epochs = 30               # number of epochs to train models
-num_src_epochs = 15           # number of epochs to pre-train source only model
-batch_size = 50               # size of image sample per epoch 
-source_data = 'mnist'         # mnist / mnist_m / usps
-target_data = 'usps'          # mnist / mnist_m / usps
-input_ch = 1                  # 1 for usps, 3 for mnistm
+learning_rate = args.lr  # learning rate
+num_epochs = args.epochs  # number of epochs to train models
+num_src_epochs = args.num_src_epochs  # number of epochs to pre-train source only model
+batch_size = args.batch_size  # size of image sample per epoch
+source_data = args.source  # mnist / mnist_m / usps
+target_data = args.target  # mnist / mnist_m / usps
+input_ch = 1  # 1 for usps, 3 for mnistm
+if target_data is "mnist_m":
+    input_ch = 3
 
 # instantiate the models
 f_ext, d_clf, c_clf = DANN(input_ch)
 
 # set loss functions
-d_crit = nn.BCELoss()         # binary crossentropy
-c_crit = nn.MSELoss()         # mean squared error
+d_crit = nn.BCELoss()  # binary crossentropy
+c_crit = nn.MSELoss()  # mean squared error
 
 # set optimizers
 d_optimizer = optim.SGD(d_clf.parameters(), lr=learning_rate, momentum=0.9)
@@ -63,37 +95,38 @@ f_optimizer = optim.SGD(f_ext.parameters(), lr=learning_rate, momentum=0.9)
 
 # init necessary objects
 num_steps = num_epochs * (Xs_train.shape[0] / batch_size)
-yd = Variable(torch.from_numpy(np.hstack([np.repeat(1, int(batch_size / 2)), np.repeat(0, int(batch_size / 2))]).reshape(50, 1)))
+yd = Variable(
+    torch.from_numpy(np.hstack([np.repeat(1, int(batch_size / 2)), np.repeat(0, int(batch_size / 2))]).reshape(50, 1)))
 j = 0
 
 # pre-train source only model
 print('\nPre-training source-only model..')
 for i in range(num_src_epochs):
     source_gen = batch_generator(int(batch_size / 2), Xs_train, ys_train)
-    
+
     # iterate over batches
     for (xs, ys, _) in source_gen:
-        
+
         # exit when batch size mismatch
         if len(xs) != batch_size / 2:
             continue
-        
+
         # reset gradients
         f_ext.zero_grad()
         c_clf.zero_grad()
-        
+
         # calculate class_classifier predictions
         c_out = c_clf(f_ext(xs).view(int(batch_size / 2), -1))
-        
+
         # optimize feature_extractor and class_classifier with output
         f_c_loss = c_crit(c_out, ys.float())
-        f_c_loss.backward(retain_graph = True)
+        f_c_loss.backward(retain_graph=True)
         c_optimizer.step()
         f_optimizer.step()
-        
+
         # print batch statistics
-        print('\rEpoch {}       - loss: {}'.format(i+1, format(f_c_loss.data[0], '.4f')), end='')
-    
+        print('\rEpoch {}       - loss: {}'.format(i + 1, format(f_c_loss.data[0], '.4f')), end='')
+
     # print epoch statistics    
     s_acc = eval_clf(c_clf, f_ext, Xs_test, ys_test, 1000)
     print(' - val_acc: {}'.format(format(s_acc, '.4f')))
@@ -110,60 +143,60 @@ for i in range(num_epochs):
 
     # iterate over batches
     for (xs, ys, _) in source_gen:
-        
+
         # update lambda and learning rate as suggested in the paper
         p = float(j) / num_steps
         lambd = round(2. / (1. + np.exp(-10. * p)) - 1, 3)
-        lr = 0.01 / (1. + 10 * p)**0.75
+        lr = 0.01 / (1. + 10 * p) ** 0.75
         d_clf.set_lambda(lambd)
         d_optimizer.lr = lr
         c_optimizer.lr = lr
         f_optimizer.lr = lr
         j += 1
-        
+
         # get next target batch
         xt, _ = next(target_gen)
 
         # exit when batch size mismatch
         if len(xs) + len(xt) != batch_size:
             continue
-        
+
         # concatenate source and target batch
         x = torch.cat([xs, xt], 0)
-        
+
         # 1) train feature_extractor and class_classifier on source batch
         # reset gradients
         f_ext.zero_grad()
         c_clf.zero_grad()
-        
+
         # calculate class_classifier predictions on batch xs
         c_out = c_clf(f_ext(xs).view(int(batch_size / 2), -1))
-        
+
         # optimize feature_extractor and class_classifier on output
         f_c_loss = c_crit(c_out, ys.float())
-        f_c_loss.backward(retain_graph = True)
+        f_c_loss.backward(retain_graph=True)
         c_optimizer.step()
         f_optimizer.step()
-        
+
         # 2) train feature_extractor and domain_classifier on full batch x
         # reset gradients
         f_ext.zero_grad()
         d_clf.zero_grad()
-        
+
         # calculate domain_classifier predictions on batch x
         d_out = d_clf(f_ext(x).view(batch_size, -1))
-        
+
         # use normal gradients to optimize domain_classifier
         f_d_loss = d_crit(d_out, yd.float())
-        f_d_loss.backward(retain_graph = True)
+        f_d_loss.backward(retain_graph=True)
         d_optimizer.step()
         f_optimizer.step()
-        
+
         # print batch statistics
         print('\rEpoch         - d_loss: {} - c_loss: {}'.format(format(f_d_loss.data[0], '.4f'),
-                            format(f_c_loss.data[0], '.4f')), end='')           
-    
-    # print epoch statistics
+                                                                 format(f_c_loss.data[0], '.4f')), end='')
+
+        # print epoch statistics
     t_acc = eval_clf(c_clf, f_ext, Xt_test, yt_test, 1000)
     s_acc = eval_clf(c_clf, f_ext, Xs_test, ys_test, 1000)
     print(' - target_acc: {} - source_acc: {}'.format(format(t_acc, '.4f'), format(s_acc, '.4f')))
